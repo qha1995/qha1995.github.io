ada068ef9ea2239b75b99d2354d48f58ba5d880c
qha1995 <105771050+qha1995@users.noreply.github.com>
2022-05-23 17:51:23 +0900

    Add files via upload

---
+++ document/trn01/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $('.first_slider').slick({
+        arrows: false, //버튼 없애주는 
+        autoplay: true, //자동 재생하는
+        dots: true,
+    });
+
+})
+//document.querySelector = $
+//document.quertSeclector('h1').style.display='none';
