31b705d981e907af8dddd5b63f03de4ef27d8464
qha1995 <105771050+qha1995@users.noreply.github.com>
2022-05-25 17:22:28 +0900

    Add files via upload

--- document/trn01/js/main.js
+++ document/trn01/js/main.js
@@ -2,12 +2,19 @@
 //autoplay: true, 자동 재생하는
 //document.querySelector = $
 //document.quertSeclector('h1').style.display='none';
-
 $(function () {
+    $('.main_slider').on('init reInit afterChange', function () {
+        var here = $('.slick-current');
+        here.addClass('on').siblings().removeClass('on')
+
+    })
+
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
         pauseOnHover: false,
         pauseOnFocus: false,
+        fade: true,
     })
 })
+
