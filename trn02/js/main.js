229709feb36f9de87a82b84e98916a376229e637
qha1995 <105771050+qha1995@users.noreply.github.com>
2022-06-20 15:39:35 +0900

    Add files via upload

---
+++ trn02/js/main.js
@@ -0,0 +1,31 @@
+$(function () {
+    //20220620 choi topBanner
+    $('.topBanner i').on('click', function () {
+        $('.topBanner').slideUp();
+    });
+    $('.mainSlider').on('init afterChange', function (e, s, c) {
+        var current = $('.slick-current');
+        current.addClass('on').siblings().removeClass('on');
+        if (c === 1) {
+            $('.mainArrow i').removeClass('on');
+        }
+        $('.slideNum').text((c ? (c + 1) : 1) + " / " + s.slideCount);
+
+    });
+
+    $('.mainSlider').slick({
+        arrows: false,
+        //autoplay: true,
+        dots: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+
+
+    });
+    $('.mainArrow i:nth-child(1)').on('click', function () {
+        $('.mainSlider').slick('slickPrev')
+    });
+    $('.mainArrow i:nth-child(2)').on('click', function () {
+        $('.mainSlider').slick('slickNext')
+    });
+})
